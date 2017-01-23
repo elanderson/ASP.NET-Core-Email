8d21d7272c1339bb0247650dc5c7e88524d5c52f
Eric Anderson <elanderson@gmail.com>
2017-01-23 06:34:42 -0600

    Initial add of project

---
+++ ASP.NET-Core-Email/src/ASP.NET-Core-Email/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Email.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}

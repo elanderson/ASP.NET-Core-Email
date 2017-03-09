c4b534ee2154d21def4d786d589206b3b69eb0a6
Eric Anderson <elanderson@gmail.com>
2017-03-09 05:23:18 -0600

    Migrate to csproj

--- ASP.NET-Core-Email/src/ASP.NET-Core-Email/Startup.cs
+++ ASP.NET-Core-Email/src/ASP.NET-Core-Email/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ASP.NET_Core_Email.Configuration;
+﻿using ASP.NET_Core_Email.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
